607096b78779cceac91070fe027f28e9219b4f48
Mat-404 <cotem2021@my.fit.edu>
2022-12-27 11:55:49 -0500

    Improved Formatting

--- getAPIData.js
+++ getAPIData.js
@@ -1,4 +1,12 @@
 const regex = /(https?:\/\/[^\s]+)/g;
+var options = {
+  timeZone: 'America/New_York',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+}
 
 // Function to display GitHub repositories in a Bootstrap table
 function displayGitHubRepos(data) {
@@ -21,20 +29,21 @@ function displayRocketLaunches(data) {
   for (const item of data.result) {
     if (item.win_open == null) {
       if (item.t0 == null) {
-        var formattedDate = new Date(item.date_str).toLocaleDateString(); // format the date
+        var formattedDate = new Date(item.date_str).toLocaleString('en-US', options); // format the date
       } else {
-        var formattedDate = new Date(item.t0).toLocaleDateString();
+        var formattedDate = new Date(item.t0).toLocaleString('en-US', options);
       }
     }
     else {
-      var formattedDate = new Date(item.win_open).toLocaleDateString(); // format the date
+      var formattedDate = new Date(item.win_open).toLocaleString('en-US', options); // format the date
     }
+    formattedDate = formattedDate.replace(/ at /, '<br>at ');
     string = item.quicktext;
     url = string.match(regex)[0];
     statusHTML += '<tr>';
     statusHTML += '<td>' + '<a href='+ url +'>' + item.name + '</a>' + '</td>';
     statusHTML += '<td>' + item.provider.name + '</td>';
-    statusHTML += '<td>' + item.pad.name + ', ' + item.pad.location.country + '</td>';
+    statusHTML += '<td>' + item.pad.name + '<br>' + item.pad.location.country + '</td>';
     statusHTML += '<td class="stateLocation">' + String(item.pad.location.state) + '</td>';
     statusHTML += '<td>' + formattedDate + '</td>';
     statusHTML += '</tr>';
@@ -73,13 +82,11 @@ otherRequest.onload = function () {
 // Send request
 otherRequest.send();
 
-var rows = document.querySelectorAll("#rocketTable tr");
-console.log(rows)
-for (const row of rows){
-  var statusCell = row.querySelector('.stateLocation');
-  var state = statusCell.textContent;
-  console.log(state);
-  if (state === 'FL') {
-    row.classList.add('fromFlorida');
-  }
-}
+$(document).ready(function() {
+  $('.table td:nth-child(4)').each(function() {
+    if ($(this).text() == 'FL') {
+      $(this).parent().css('background-color', '#97dfe6');
+      $(this).parent().css('font-weight', 'bold');
+    }
+  });
+});
